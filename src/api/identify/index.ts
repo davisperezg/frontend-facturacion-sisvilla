9f8f0dfe0f73c8730f367f531196d1991855a28c
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2022-02-04 16:18:38 -0500

    api para buscar personas por dni y ruc resuleto

--- src/api/identify/index.ts
+++ src/api/identify/index.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
+import { API } from "../../lib/consts/const";
 
 export const getServiceData = (type: string, value: string) => {
-  return axios.get(`http://localhost:3001/api/v1/${type}?numero=${value}`, {
-    headers: {
-      Authorization: `Bearer apis-token-1541.jEQrwuQ-d3dwGzF4B627X15xuNEN2mio`,
-    },
-  });
+  return axios.get(`${API}/api/v1/users/services/${type}/${value}`);
 };
