82004e166f9f16451b8ecb664c554aecf3e40768
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2022-02-14 13:35:47 -0500

    modificaciones para un demo

--- src/components/HeaderComponent/Header.tsx
+++ src/components/HeaderComponent/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
       <div className={styles.item}>
         <div className={styles.item__left}>
           <h1>
-            Comercial <strong>SARAI</strong>
+            TU <strong>EMPRESA</strong>
           </h1>
         </div>
       </div>
